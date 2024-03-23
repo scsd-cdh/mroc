6f5379a9268858a1327be8bf1d60be5c9b059e63
Ruben1729 <ruben.e.sanchez.a@gmail.com>
2024-03-22 22:12:12 -0400

    Pinout for board rev 2

--- src/config.h
+++ src/config.h
@@ -30,38 +30,38 @@
 #define MAIN_SLAVE_ADDR         0x48
 
 #define REDUNDANT_I2C_SDA       GPIO_PIN1
-#define REDUNDANT_I2C_SCL       GPIO_PIN2
-#define REDUNDANT_I2C_PORT      GPIO_PORT_P3
+#define REDUNDANT_I2C_SCL       GPIO_PIN1
+#define REDUNDANT_I2C_SDA_PORT  GPIO_PORT_P3
+#define REDUNDANT_I2C_SCL_PORT  GPIO_PORT_p4
 #define REDUNDANT_SLAVE_ADDR    0x48
 
 // Bipump Pinout
-#define BIPUMP_Q2_PORT          GPIO_PORT_P2
-#define BIPUMP_Q2_PIN           GPIO_PIN0
-#define BIPUMP_Q1_PORT          GPIO_PORT_P2
-#define BIPUMP_Q1_PIN           GPIO_PIN1
-#define BIPUMP_Q6_PORT          GPIO_PORT_P2
-#define BIPUMP_Q6_PIN           GPIO_PIN2
-#define BIPUMP_Q5_PORT          GPIO_PORT_P2
-#define BIPUMP_Q5_PIN           GPIO_PIN3
+#define BIPUMP_Q2_PORT          GPIO_PORT_P9
+#define BIPUMP_Q2_PIN           GPIO_PIN2
+#define BIPUMP_Q1_PORT          GPIO_PORT_P9
+#define BIPUMP_Q1_PIN           GPIO_PIN3
+#define BIPUMP_Q6_PORT          GPIO_PORT_P9
+#define BIPUMP_Q6_PIN           GPIO_PIN4
+#define BIPUMP_Q5_PORT          GPIO_PORT_P9
+#define BIPUMP_Q5_PIN           GPIO_PIN5
 
 // Pump Pinout
 #define PUMP_PORT               GPIO_PORT_P3
-#define PUMP_PIN                GPIO_PIN7
+#define PUMP_PIN                GPIO_PIN3
 
 // BME280 Pinout
 
 // LMT01 Pinout
-#define LMT01_PORT              GPIO_PORT_P5
-#define LMT01_PIN               GPIO_PIN0
+#define LMT01_PORT              GPIO_PORT_P2
+#define LMT01_PIN               GPIO_PIN7
 
 // Valve Pinout
-#define VALVE_PORT              GPIO_PORT_P3
-#define VALVE_PIN               GPIO_PIN6
+#define VALVE_PORT              GPIO_PORT_P2
+#define VALVE_PIN               GPIO_PIN1
 
 // Heater Pinout
-#define HEATER_PORT             GPIO_PORT_P9
-#define HEATER_PIN              GPIO_PIN6
-
+#define HEATER_PORT             GPIO_PORT_P5
+#define HEATER_PIN              GPIO_PIN0
 
 // MPQ3326 Pinout
 #define MPQ3326_W570_ADDR       0x36
@@ -83,14 +83,14 @@
 #define OPT4003_I2C_ADDR_3      0x46
 #define OPT4003_I2C_ADDR_4      0x47
 
-#define OPT4003_I2C_SDA_1_PxOUT P9OUT
-#define OPT4003_I2C_SDA_1_PxIN  P9IN
-#define OPT4003_I2C_SDA_1_PxDIR P9DIR
-#define OPT4003_I2C_SDA_1_PIN   GPIO_PIN4
-#define OPT4003_I2C_SCL_1_PxOUT P9OUT
-#define OPT4003_I2C_SCL_1_PxIN  P9IN
-#define OPT4003_I2C_SCL_1_PxDIR P9DIR
-#define OPT4003_I2C_SCL_1_PIN   GPIO_PIN2
+#define OPT4003_I2C_SDA_1_PxOUT P2OUT
+#define OPT4003_I2C_SDA_1_PxIN  P2IN
+#define OPT4003_I2C_SDA_1_PxDIR P2DIR
+#define OPT4003_I2C_SDA_1_PIN   GPIO_PIN2
+#define OPT4003_I2C_SCL_1_PxOUT P2OUT
+#define OPT4003_I2C_SCL_1_PxIN  P2IN
+#define OPT4003_I2C_SCL_1_PxDIR P2DIR
+#define OPT4003_I2C_SCL_1_PIN   GPIO_PIN3
 
 #define OPT4003_I2C_SDA_2_PxOUT P1OUT
 #define OPT4003_I2C_SDA_2_PxIN  P1IN
@@ -104,19 +104,19 @@
 #define OPT4003_I2C_SDA_3_PxOUT P9OUT
 #define OPT4003_I2C_SDA_3_PxIN  P9IN
 #define OPT4003_I2C_SDA_3_PxDIR P9DIR
-#define OPT4003_I2C_SDA_3_PIN   GPIO_PIN1
+#define OPT4003_I2C_SDA_3_PIN   GPIO_PIN7
 #define OPT4003_I2C_SCL_3_PxOUT P9OUT
 #define OPT4003_I2C_SCL_3_PxIN  P9IN
 #define OPT4003_I2C_SCL_3_PxDIR P9DIR
-#define OPT4003_I2C_SCL_3_PIN   GPIO_PIN0
+#define OPT4003_I2C_SCL_3_PIN   GPIO_PIN6
 
 #define OPT4003_I2C_SDA_4_PxOUT P3OUT
 #define OPT4003_I2C_SDA_4_PxIN  P3IN
 #define OPT4003_I2C_SDA_4_PxDIR P3DIR
-#define OPT4003_I2C_SDA_4_PIN   GPIO_PIN1
+#define OPT4003_I2C_SDA_4_PIN   GPIO_PIN6
 #define OPT4003_I2C_SCL_4_PxOUT P3OUT
 #define OPT4003_I2C_SCL_4_PxIN  P3IN
 #define OPT4003_I2C_SCL_4_PxDIR P3DIR
-#define OPT4003_I2C_SCL_4_PIN   GPIO_PIN2
+#define OPT4003_I2C_SCL_4_PIN   GPIO_PIN5
 
 #endif /* SRC_CONFIG_H_ */
