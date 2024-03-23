bcc4c46ebdf1a905e0b8d1fc5302788e03780f78
Ruben1729 <ruben.e.sanchez.a@gmail.com>
2024-03-23 00:46:59 -0400

    Remove original swi2c implementation

--- src/peripherals/bme280.c
+++ src/peripherals/bme280.c
@@ -44,10 +44,24 @@ int8_t BME280_Init(BME280_Descriptor *descriptor) {
     );
 
     GPIO_setAsPeripheralModuleFunctionInputPin(
-            descriptor->mosi_port,
-            descriptor->mosi_pin,
-            GPIO_SECONDARY_MODULE_FUNCTION
-        );
+        descriptor->mosi_port,
+        descriptor->mosi_pin,
+        GPIO_SECONDARY_MODULE_FUNCTION
+    );
+
+    EUSCI_A_SPI_initMasterParam param = {0};
+    param.selectClockSource = EUSCI_A_SPI_CLOCKSOURCE_SMCLK;
+    param.clockSourceFrequency = CS_getSMCLK();
+    param.desiredSpiClock = 500000;
+    param.msbFirst = EUSCI_A_SPI_MSB_FIRST;
+    param.clockPhase = EUSCI_A_SPI_PHASE_DATA_CHANGED_ONFIRST_CAPTURED_ON_NEXT;
+    param.clockPolarity = EUSCI_A_SPI_CLOCKPOLARITY_INACTIVITY_HIGH;
+    param.spiMode = EUSCI_A_SPI_3PIN;
+    EUSCI_A_SPI_initMaster(EUSCI_A0_BASE, &param);
+    EUSCI_A_SPI_enable(EUSCI_A0_BASE);
+    EUSCI_A_SPI_clearInterrupt(EUSCI_A0_BASE, EUSCI_A_SPI_RECEIVE_INTERRUPT);
+    EUSCI_A_SPI_enableInterrupt(EUSCI_A0_BASE, EUSCI_A_SPI_RECEIVE_INTERRUPT);
+
     return 0;
 }
 
